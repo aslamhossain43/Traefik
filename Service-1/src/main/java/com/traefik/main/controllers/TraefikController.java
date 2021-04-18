77131225c1b0e1c66e5f575e9d48481ad539c0b2
Aslam-Hossain <aslamecehstu43java@gmail.com>
2021-04-18 23:40:31 +0600

    Fixed traefik proxy

---
+++ Service-1/src/main/java/com/traefik/main/controllers/TraefikController.java
@@ -0,0 +1,12 @@
+package com.traefik.main.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TraefikController {
+	@RequestMapping(value = "/")
+	public String saySomething() {
+		return "From service-1";
+	}
+}
---
+++ Service-3/src/main/java/com/traefik/main/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.traefik.main;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Service3Application.class);
+	}
+
+}
