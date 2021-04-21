5b395a0e589bf7ae126e780b4b92b374b307259c
Aslam-Hossain <aslamecehstu43java@gmail.com>
2021-04-22 04:11:16 +0600

    Consul is added

--- Service-1/src/main/java/com/traefik/main/Service1Application.java
+++ Service-1/src/main/java/com/traefik/main/Service1Application.java
@@ -2,9 +2,11 @@ package com.traefik.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = {"com.traefik.main.controllers"})
 public class Service1Application {
 
--- Service-4/src/main/java/com/traefik/main/Service4Application.java
+++ Service-4/src/main/java/com/traefik/main/Service4Application.java
@@ -2,8 +2,12 @@ package com.traefik.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.traefik.main.controllers"})
 public class Service4Application {
 
 	public static void main(String[] args) {
